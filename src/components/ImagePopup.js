2832443aca96fe02d8e65dd2a3c5d55a14508ad1
obuzmakova <olga.buzmakova@gmail.com>
2021-05-08 16:33:15 +0300

    fix src in img tag

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -5,7 +5,7 @@ function ImagePopup({card, onClose}) {
         <div className={`popup popup_type_img ${card ? `popup_opened` : ""}`}>
             <div className="popup__container">
                 <button type="button" className="popup__close-button" onClick={onClose}/>
-                <img className="popup__image" src={card.link} alt={card.name}/>
+                <img className="popup__image" src={`${card.link}`} alt={card.name}/>
                 <h3 className="popup__name-place">{card.name}</h3>
             </div>
         </div>
