1e44877e62fbc85e6b25123f63fe96336d4c87d3
obuzmakova <olga.buzmakova@gmail.com>
2021-05-12 22:58:21 +0300

    add alt for card image

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ function Card(props) {
 
     return (
         <div className="element">
-            <img className="element__photo" src={props.link} onClick={handleClick} alt="#"/>
+            <img className="element__photo" src={props.link} onClick={handleClick} alt="{props.name}"/>
             <h3 className="element__title">{props.name}</h3>
             <button type="button" className="element__like"/>
             <p className="element__counter">{props.likes.length}</p>
